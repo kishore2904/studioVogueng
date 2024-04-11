5d5350823f4420157b8c9bbaf731a8eea57b57d7
Kishore <kishore290499@gmail.com>
2024-04-12 00:10:36 +0530

    categories page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+import { NgIf } from '@angular/common';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    NgIf,
+    CardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,33 +3,63 @@ import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth.service';
 import { NgIf } from '@angular/common';
-
+import { Product } from '../../model/product';
+import { ProductService } from '../../services/productservice';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule,NgIf],
+  imports: [ButtonModule,NgIf,CarouselModule],
+  providers:[ProductService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements  OnInit {
   isLogOut: boolean= false;
-  constructor(private router: Router,
-    private authService: AuthService) {}
-  ngOnInit(): void {
-    this.authService.user$.subscribe(user=>{
-      if(user){
-        this.isLogOut = true;
-        this.authService.currentUserSignal.set({
-          email: user.email!,
-          userName: user.displayName!,
+  products: Product[] =[];
 
-        });
-      }else{
-        this.authService.currentUserSignal.set(null);
-      }
-      console.log(this.authService.currentUserSignal());
-    })
+    responsiveOptions: any[] | undefined;
+  constructor(private router: Router,
+    private authService: AuthService,
+    private productService: ProductService) {}
+  ngOnInit(): void {
+    // this.authService.user$.subscribe(user=>{
+    //   if(user){
+    //     this.isLogOut = true;
+    //     this.authService.currentUserSignal.set({
+    //       email: user.email!,
+    //       userName: user.displayName!,
+
+    //     });
+    //   }else{
+    //     this.authService.currentUserSignal.set(null);
+    //   }
+    //   console.log(this.authService.currentUserSignal());
+    // });
+
+    this.productService.getProductsSmall().then((products) => {
+        this.products = products;
+    });
+
+    this.responsiveOptions = [
+        {
+            breakpoint: '1199px',
+            numVisible: 1,
+            numScroll: 1
+        },
+        {
+            breakpoint: '991px',
+            numVisible: 2,
+            numScroll: 1
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];
+
   }
 
   navigateToLogin(){
@@ -38,6 +68,9 @@ export class HomeComponent implements  OnInit {
   logOut(){
     this.authService.logout();
     this.router.navigate(['/home']);
+  }
+  getSeverity(status:string){
+console.log(status);
   }
 
 }
